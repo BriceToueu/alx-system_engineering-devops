839465abc5c42e90a659ebdfdc7ac38bf6bb75bc
Brice Toueu <guy-brice.toueu@genesis.ci>
2025-01-10 15:13:35 -0800

    second task updated

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,15 @@
-# Installs puppet-lint
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => ['/usr/bin', '/usr/sbin'],
+}
 
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+exec { 'install_werkzeug':
+  command => '/usr/bin/pip3 install werkzeug==2.1.1',
+  path    => ['/usr/bin', '/usr/sbin'],
+}
+
+exec { 'verify_flask_install':
+  command => '/usr/local/bin/flask --version',
+  path    => ['/usr/bin', '/usr/sbin'],
+  require => Exec['install_flask', 'install_werkzeug'],
 }
